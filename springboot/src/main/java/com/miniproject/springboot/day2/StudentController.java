cf5d9ba6346a540101017c583397602f397bd81e
sherin <sherinshaik9849@gmail.com>
2026-09-19 22:45:20 +0530

    Day 2 - Added students list API

--- springboot/src/main/java/com/miniproject/springboot/day2/StudentController.java
+++ springboot/src/main/java/com/miniproject/springboot/day2/StudentController.java
@@ -1,14 +1,24 @@
 package com.miniproject.springboot.day2;
-
+import java.util.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
-    @GetMapping("/StudentDetails")
+    /*@GetMapping("/StudentDetails")
    public Student getStudentDetails(){
      Student s = new Student(2,"sherin","NECN","CSE-AIML");
-   
-   return s;
+   return s; 
+  }--for only one object*/
+
+  /*for list of objects */
+  @GetMapping("/StudentsDetails")
+  public List<Student> getStudentDetails(){
+            List<Student> s = new ArrayList<>();
+            s.add(new Student(
+              1,"sherin" ,"necn" ,"aiml" 
+            ));
+            s.add(new Student(2,"ravi" , "gist", "cse"));
+        return s;
   }
 }
