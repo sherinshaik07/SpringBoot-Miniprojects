452021405312d98428851e3d2d5386baf8e1bf78
sherin <sherinshaik9849@gmail.com>
2026-09-19 23:47:43 +0530

    worked on postmapping,requestbody

--- springboot/src/main/java/com/miniproject/springboot/day2/Student.java
+++ springboot/src/main/java/com/miniproject/springboot/day2/Student.java
@@ -5,13 +5,19 @@ public class Student {
     private String name;
     private String college;
     private String course;
-
+//parameterized constructor used for GET APIs
     public Student(int id,String name,String college,String course){
         this.id = id;
         this.name=name;
         this.college=college;
         this.course=course;
     }
+
+    //Default constructor for @RequestBody
+    public Student(){
+
+    }
+    //getters
     public int getId(){
         return id;
     }
@@ -24,5 +30,19 @@ public class Student {
     public String getCourse(){
         return course;
     }
+    //setters
+    public void setId(int id){
+        this.id=id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setCollege(String college){
+        this.college = college;
+    }
+    public void setCourse(String course){
+        this.course = course;
+    }
+
     
 }
--- springboot/src/main/java/com/miniproject/springboot/day2/StudentController.java
+++ springboot/src/main/java/com/miniproject/springboot/day2/StudentController.java
@@ -1,20 +1,21 @@
 package com.miniproject.springboot.day2;
 import java.util.*;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import  org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class StudentController {
-  @GetMapping("/StudentsDetails/{id}")
-  public Student getStudentDetails(@PathVariable int id){
-           if(id==1){
-            return new Student(
-              1,"sherin" ,"necn" ,"aiml" 
-            );}
-           else if(id==2){
-           return new Student(2,"ravi" , "gist", "cse");
-           }
-        return null;
+  /*@Requestparam is used to get a value from urls query parameter 
+  EX : GET/searchStudents/name?sherin */
+  @GetMapping("/searchStudents")
+  public String searchStudents(@RequestParam String name){
+          return "Searching for student: "+name;
+  }
+  @PostMapping("/students")
+  public Student addStudent(@RequestBody Student s){
+    return s;
   }
 }
