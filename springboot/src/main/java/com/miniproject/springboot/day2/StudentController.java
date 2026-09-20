c78912b8ed85cae8f519477bc647cef845604f81
sherin <sherinshaik9849@gmail.com>
2026-09-20 23:53:33 +0530

    DAY 2 - put,delete

--- springboot/src/main/java/com/miniproject/springboot/day2/StudentController.java
+++ springboot/src/main/java/com/miniproject/springboot/day2/StudentController.java
@@ -1,21 +1,26 @@
 package com.miniproject.springboot.day2;
 import java.util.*;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import  org.springframework.web.bind.annotation.RequestParam;
+import  org.springframework.web.bind.annotation.PathVariable;
+import  org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class StudentController {
-  /*@Requestparam is used to get a value from urls query parameter 
-  EX : GET/searchStudents/name?sherin */
-  @GetMapping("/searchStudents")
-  public String searchStudents(@RequestParam String name){
-          return "Searching for student: "+name;
+      @PutMapping("/students/{id}")
+      public Student updateStudent(@PathVariable int id,@RequestBody Student s){
+           s.setId(id);
+           return s;
+      }
+      @DeleteMapping("/studentsdelete/{id}")
+      public String DeleteStudent(@PathVariable int id){
+        return "Deleted "+id+ "successfully";
+      }
+
   }
-  @PostMapping("/students")
-  public Student addStudent(@RequestBody Student s){
-    return s;
-  }
-}
+
